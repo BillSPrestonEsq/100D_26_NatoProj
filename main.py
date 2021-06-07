65f17d80521e8b476f158b211274ab96d31f4a8b
BillSPrestonEsq <58013891+BillSPrestonEsq@users.noreply.github.com>
2021-06-07 13:47:52 -0400

    Final

--- main.py
+++ main.py
@@ -1,27 +1,49 @@
-student_dict = {
-    "student": ["Angela", "James", "Lily"],
-    "score": [56, 76, 98]
-}
-
-#Looping through dictionaries:
-for (key, value) in student_dict.items():
-    #Access key and value
-    pass
+# # student_dict = {
+# #     "student": ["Angela", "James", "Lily"],
+# #     "score": [56, 76, 98]
+# # }
+# #
+# # #Looping through dictionaries:
+# # for (key, value) in student_dict.items():
+# #     #Access key and value
+# #     pass
+# #
+# # import pandas
+# # student_data_frame = pandas.DataFrame(student_dict)
+# #
+# # #Loop through rows of a data frame
+# # for (index, row) in student_data_frame.iterrows():
+# #     #Access index and row
+# #     #Access row.student or row.score
+# #     pass
+# #
+# # # Keyword Method with iterrows()
+# # # {new_key:new_value for (index, row) in df.iterrows()}
+#
+# #TODO 1. Create a dictionary in this format:
+# #{"A": "Alfa", "B": "Bravo"}
+# with open("nato_phonetic_alphabet.csv") as file:
+#     nato_list = file.read().splitlines()
+#
+# nato_dict = {pair[:pair.index(",")]:pair[pair.index(",") + 1:] for pair in nato_list}
+#
+# print(nato_dict)
+#
+# #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+# user_input = input("What would you like to translate to the Nato alphabet? ")
+#
+# print([nato_dict[letter] for letter in user_input.upper()])
 
 import pandas
-student_data_frame = pandas.DataFrame(student_dict)
-
-#Loop through rows of a data frame
-for (index, row) in student_data_frame.iterrows():
-    #Access index and row
-    #Access row.student or row.score
-    pass
-
-# Keyword Method with iterrows()
-# {new_key:new_value for (index, row) in df.iterrows()}
-
 #TODO 1. Create a dictionary in this format:
-{"A": "Alfa", "B": "Bravo"}
+#{"A": "Alfa", "B": "Bravo"}
+df = pandas.read_csv("nato_phonetic_alphabet.csv")
+
+nato_dict = {row.letter:row.code for (index, row) in df.iterrows()}
+
+#print(nato_dict)
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+user_input = input("What would you like to translate to the Nato alphabet? ")
 
+print([nato_dict[letter] for letter in user_input.upper()])
